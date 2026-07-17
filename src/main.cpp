f29da5ed0e99a06f51a66dce7b5445d1b4ae7e72
present777 <2509372195@qq.com>
2026-07-17 16:40:24 +0800

    Update main to test Monte Carlo pricing

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "../include/Payoff.hpp"
 #include "../include/VanillaOption.hpp"
 #include "../include/BlackScholes.hpp"
+#include "../include/MonteCarlo.hpp"
 
 int main(){
     double S=100.0;
@@ -11,9 +12,17 @@ int main(){
     double sigma=0.2;
     double T=1.0;
 
+    PayoffCall payoff(K);
+    VanillaOption option(T,payoff);
+
+    int num_paths=1000000;
+
     double bs_price=blackscholesCallPrice(S,K,r,T,sigma);
+    double mc_price=MonteCarloPrice(option,S,r,sigma,num_paths);
 
     std::cout<<"Black-Scholes Call Price: "<< bs_price <<std::endl;
+    std::cout << "========================================" << std::endl;
+    std::cout<<"Monte Carlo Call Price: "<< mc_price <<std::endl;
 
     return 0;
 }
