eaa731ad6f5333650983a35b5e8422cd7e9d3a19
present777 <2509372195@qq.com>
2026-07-17 19:29:38 +0800

    Update main to test Binomial Tree

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "../include/VanillaOption.hpp"
 #include "../include/BlackScholes.hpp"
 #include "../include/MonteCarlo.hpp"
+#include "../include/BinomialTree.hpp"
 
 int main(){
     double S=100.0;
@@ -16,13 +17,17 @@ int main(){
     VanillaOption option(T,payoff);
 
     int num_paths=1000000;
+    int steps=1000;
 
     double bs_price=blackscholesCallPrice(S,K,r,T,sigma);
     double mc_price=MonteCarloPrice(option,S,r,sigma,num_paths);
+    double bt_price=BinomialTreePrice(option,S,r,sigma,steps);
 
     std::cout<<"Black-Scholes Call Price: "<< bs_price <<std::endl;
     std::cout << "========================================" << std::endl;
     std::cout<<"Monte Carlo Call Price: "<< mc_price <<std::endl;
+    std::cout << "========================================" << std::endl;
+    std::cout<<"Binomial Tree Call Price: "<< bt_price <<std::endl;
 
     return 0;
 }
