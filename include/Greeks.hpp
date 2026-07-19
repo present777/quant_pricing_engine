fc1101ca8fe0e187a49544b8ddb2a9c801f9eef2
present777 <2509372195@qq.com>
2026-07-20 01:58:32 +0800

    Modify the step size

--- include/Greeks.hpp
+++ include/Greeks.hpp
@@ -3,6 +3,6 @@
 #include "VanillaOption.hpp"
 #include "Market.hpp"
 
-double calDelta(const VanillaOption& option, const Market& market, double dS = 1e-4);
-double calGamma(const VanillaOption& option, const Market& market, double dS = 1e-4);
+double calDelta(const VanillaOption& option, const Market& market, double dS_ratio = 1e-4);
+double calGamma(const VanillaOption& option, const Market& market, double dS_ratio = 1e-4);
 double calVega(const VanillaOption& option, const Market& market, double dsigma = 1e-4);
--- src/Greeks.cpp
+++ src/Greeks.cpp
@@ -1,33 +1,37 @@
 #include "../include/Greeks.hpp"
 #include "../include/BlackScholes.hpp"
 
-double calDelta(const VanillaOption& option, const Market& market, double dS) {
+double calDelta(const VanillaOption& option, const Market& market, double dS_ratio) {
     double S = market.getSpot();
     double r = market.getRate();
     double sigma = market.getVol();
 
-    Market env_up(S + dS, r, sigma);
-    Market env_down(S - dS, r, sigma);
+    double actual_dS = S*dS_ratio;
+
+    Market env_up(S + actual_dS, r, sigma);
+    Market env_down(S - actual_dS, r, sigma);
     
     double price_up = blackscholesPrice(option, env_up);
     double price_down = blackscholesPrice(option, env_down);
 
-    return (price_up - price_down) / (2.0 * dS);
+    return (price_up - price_down) / (2.0 * actual_dS);
 }
 
-double calGamma(const VanillaOption& option, const Market& market, double dS) {
+double calGamma(const VanillaOption& option, const Market& market, double dS_ratio) {
     double S = market.getSpot();
     double r = market.getRate();
     double sigma = market.getVol();
 
-    Market env_up(S + dS, r, sigma);
-    Market env_down(S - dS, r, sigma);
+    double actual_dS = S*dS_ratio;
+
+    Market env_up(S + actual_dS, r, sigma);
+    Market env_down(S - actual_dS, r, sigma);
     
     double price_up = blackscholesPrice(option, env_up);
     double price_mid = blackscholesPrice(option, market);
     double price_down = blackscholesPrice(option, env_down);
 
-    return (price_up - 2.0 * price_mid + price_down) / (dS * dS);
+    return (price_up - 2.0 * price_mid + price_down) / (actual_dS * actual_dS);
 }
 
 double calVega(const VanillaOption& option, const Market& market, double dsigma) {
